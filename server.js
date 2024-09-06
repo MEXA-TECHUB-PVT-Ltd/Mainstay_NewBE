a0637c02140831b8e6af1ad4dad92580fa647548
Developer1 Mtechub <developer1.mtechub@gmail.com>
2024-09-06 11:53:39 +0500

    Update server.js

--- server.js
+++ server.js
@@ -135,7 +135,7 @@ app.get("/email", async (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port =  3001;
 app.use(cors());
 app.use("/api/coachee", coachee);
 app.use("/api/coach", coach);
